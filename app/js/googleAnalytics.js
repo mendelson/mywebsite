cbc3e668796b8d74befd709ece79f0810ad80333
Mateus Mendelson <m.mendelson.unb@gmail.com>
2017-05-27 00:14:27 -0300

    Update googleAnalytics.js

--- app/js/googleAnalytics.js
+++ app/js/googleAnalytics.js
@@ -1,14 +1,3 @@
-// Basic Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-// Production
-ga('create', 'UA-40476736-3', 'auto');
-// Testing
-// ga('create', 'UA-40476736-2', 'auto');
-
 // Footer icons
 function iconClickLindekin () {
   ga('send', {
